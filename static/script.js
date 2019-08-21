3fae9b35e3a37b9ce23685bbecc7648310cf9d3a
null-null-programming <nullnullprogramming@gmail.com>
2019-08-21 23:42:31 +0900

    commit

--- static/script.js
+++ static/script.js
@@ -1,35 +1,3 @@
-(async function() {
-
-    const contestName = [];
-    $.ajax({
-        type: 'GET',
-        url: 'https://kenkoooo.com/atcoder/resources/contests.json',
-        dataType: 'json',
-        success: function(json) {
-            var len = json.length;
-            for (var i = 0; i < len; i++) {
-                contestName.push(json[i].title);
-            }
-        }
-    });
-
-    //検索候補を表示する。
-    $('#autocomplete_search').autocomplete({
-        source: contestName,
-        autoFocus: true,
-        delay: 100,
-        minLength: 2,
-        appendTo: "menu",
-
-        //候補をクリックすることでsubmitできるようにする
-        select: function(event, ui) {
-            $("#autocomplete_search").val(ui.item.label);
-            $("#search").submit();
-        }
-    });
-
-})();
-
 //input内でのEnter無効化
 $(function() {
     $(document).on("keypress", "input:not(.allow_submit)", function(event) {
