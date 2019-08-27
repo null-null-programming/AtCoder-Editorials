1ff5493471b4b49354978578d571e8ab1c2fb998
null-null-programming <nullnullprogramming@gmail.com>
2019-08-27 16:37:13 +0900

    fix

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def contest_get(problem_id,page=1):
             break
 
     #ページネーション
-    per_page = 1
+    per_page = 10
     editorials = db.session.query(Editorial).filter_by(contestname=contestname).order_by(desc(Editorial.like)).paginate(page, per_page, error_out=False)
 
     #ログインしている場合は、既にいいねしている「いいね欄」を塗りつぶす
