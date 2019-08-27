23ebfa424b16bd1f82cbc327a923540576890af5
null-null-programming <nullnullprogramming@gmail.com>
2019-08-27 19:27:06 +0900

    fix

--- app.py
+++ app.py
@@ -253,7 +253,7 @@ def oauth_callback():
     return redirect(url_for('index'))
 
 #いいね処理
-@app.route('/like',methods=['GET'])
+@app.route('/like',methods=['POST'])
 def like():
     id=request.args.get('id')
 
